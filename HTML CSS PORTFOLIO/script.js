f0055abbe5d21d9291fb4fc0b0b3e6332badf6c2
Farhan20245 <alam22205341122@diu.edu.bd>
2025-05-03 12:01:47 +0600

    Update script.js

--- HTML CSS PORTFOLIO/script.js
+++ HTML CSS PORTFOLIO/script.js
@@ -1,13 +1,10 @@
-// Hobby Data as an Array of Objects
 const hobbies = [
     { name: "Graphics Designing", image: "images/hobby1.jpg" },
     { name: "Web Designing", image: "images/hobby2.jpg" }
 ];
 
-// Select the Hobby Container
 const hobbyContainer = document.getElementById("hobby-container");
 
-// Loop through hobbies and insert into the HTML
 hobbies.forEach(hobby => {
     let hobbyDiv = document.createElement("div");
     hobbyDiv.classList.add("gallery-item");
